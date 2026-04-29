289e8f34b429636cad6583fe3286bd5d492af29b
pgmallpng <pgmallpng@gmail.com>
2026-04-29 12:14:17 +1000

    Create contact-config.js

---
+++ contact-config.js
@@ -0,0 +1,7 @@
+var CONTACT_CONFIG = {
+    hours: "10am – 10pm (PNG time)",
+    contacts: [
+        { name: "Telegram",   url: "https://t.me/Louise68110",      icon: "📱" },
+        { name: "Signal",            url: "https://signal.me/#eu/rGX1u91N_VgSE9sXx0FYh6drPhZdvCwWcf6cYrn8xzSc5oAOm_hBUrWlk2zpb59w",     icon: "💬" },
+    ]
+};
